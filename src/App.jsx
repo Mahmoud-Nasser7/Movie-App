102e51b08919e916a760c306143974a551372b59
Nasser <mahmoudnasserelshamey@gamail.com>
2025-03-16 22:09:51 +0200

    Add Play Now button and Trailer video

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import MobileNav from "./components/MobileNav"
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
-import { setBannerData ,setImageUrl } from "./store/movieSlice"
+import { setBannerData, setImageUrl } from "./store/movieSlice"
 import Footer from "./components/Footer"
+import Loading from "./components/Loading"
+
 const App = () => {
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+
   const fetchTrednings = async () => {
     try {
       const response = await axios.get('/trending/all/week');    
@@ -16,29 +20,45 @@ const App = () => {
       console.error('Error:', error);
     }
   }
+
   const fetchConfig = async () => {
     try {
       const response = await axios.get('/configuration');
-      dispatch(setImageUrl(response.data.images.secure_base_url+"original"))
+      dispatch(setImageUrl(response.data.images.secure_base_url + "original"))
     } catch (error) {
       console.error('Error:', error);
     }
   }
+
   useEffect(() => {
     fetchTrednings();
-    fetchConfig()
+    fetchConfig();
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 15000);
+
+    return () => clearTimeout(timer);
   }, []);
+
+  if (loading) {
+    return (
+      <div className="fixed inset-0 bg-black  flex items-center justify-center text-white text-2xl">
+        <Loading/>
+      </div>
+    );
+  }
+
   return (
     <div className="pb-12 lg:pb-0">
       <Header/>
       <div className="min-h-[100vh]">
-      <Outlet  />
+        <Outlet  />
       </div>
       <Footer/>
       <MobileNav/>
-
     </div>
   )
 }
 
-export default App
+export default App;
